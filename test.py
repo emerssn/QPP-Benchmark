d44d96c95aa1e27f19ffaa873bfcd289d9ee99e1
Krosvick <josesand99v@gmail.com>
2026-05-22 16:35:56 -0400

    limpieza dump

--- test.py
+++
@@ -1,6 +0,0 @@
-from tira.rest_api_client import Client
-from tira.third_party_integrations import ensure_pyterrier_is_loaded
-
-ensure_pyterrier_is_loaded()
-import pyterrier as pt
-tira = Client()
